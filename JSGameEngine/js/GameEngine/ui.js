f520eebafa4c848a09c8902cea267b26032fc481
naoise collins <naoisecollins@gmail.com>
2023-11-23 15:45:21 +0000

    added UI

--- JSGameEngine/js/GameEngine/ui.js
+++ JSGameEngine/js/GameEngine/ui.js
@@ -1,5 +1,27 @@
 class UI extends Component{
-    constructor(text,x,y, font = "20px"){
+    constructor(text,x,y, font = "20px Arial", color = "white", 
+    textAlign = "left", textBaseline = "top"){
+        super();
+        this.text = text; 
+        this.x = x; 
+        this.y = y;
+        this.font = font; 
+        this.color = color; 
+        this.textAlign = textAlign; 
+        this.textBaseline = textBaseline; 
+    }
+    draw(ctx){
+        const camera = this.gameObject.game.camera; 
 
+        ctx.font = this.font; 
+        ctx.fillStyle = this.color; 
+        ctx.textAlign = this.textAlign; 
+        ctx.textBaseline = this.textBaseline;
+
+        ctx.fillText(this.text, this.x + camera.x, this.y + camera.y);
+    }
+    setText(newText){
+        this.text = newText;
     }
 }
+export default UI;
