6f7f44c178f07b1ef566b28d38caebe858f75fd5
naoise collins <naoisecollins@gmail.com>
2023-11-27 14:56:39 +0000

    added platform

--- JSGameEngine/js/Game/platform.js
+++ JSGameEngine/js/Game/platform.js
@@ -1,3 +1,14 @@
 import GameObject from "gameobject.js";
 import Renderer from "renderer.js";
 import Physics from "physics.js";
+
+class Platform extends GameObject{
+    constructor(x,y,width,height,color = "gray"){
+        super(x,y);
+        this.addComponent(new Renderer(color, width, height));
+        this.addComponent(new Physics({x:0,y:0}, {x:0, y:0}, {x:0, y:0}));
+
+        this.tag = "platform"
+    }
+}
+export default Platform;
